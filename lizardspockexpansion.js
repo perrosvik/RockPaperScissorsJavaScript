f5de3791a51bc467484c32a33f2922eafebba8a8
perrosvik <per.magne.rosvik@gmail.com>
2015-11-08 15:46:46 +0100

    Github pages

---
+++ lizardspockexpansion.js
@@ -0,0 +1,74 @@
+var winsEx= 0;
+var lossEx= 0;
+var drawsEx = 0;
+
+function startExpansion() {
+    var randomEx = Math.floor(Math.random() * 5 + 1);
+    var computerChoiceEx = computerChooseExpansion(randomEx);
+    var userChoiceEx = null;
+
+    var radiosEx = document.getElementsByName('expansion');
+    for (var i = 0, length = radiosEx.length; i < length; i++) {
+        if (radiosEx[i].checked) {
+            userChoiceEx = radiosEx[i].value;
+        }
+    }
+
+    if (userChoiceEx === null) {
+        document.getElementById('userChoiceEx').innerHTML = 'Please choose an option!';
+        document.getElementById('userChoiceEx').style.color = 'red';
+    } else {
+        document.getElementById('userChoiceEx').innerHTML = 'You chose: ' + userChoiceEx;
+        document.getElementById('aiChoiceEx').innerHTML = 'The computer chose: ' + computerChoiceEx;
+
+        var winnerEx = compareExpansion(userChoiceEx, computerChoiceEx);
+        document.getElementById('resEx').innerHTML = winnerEx;
+        document.getElementById('winEx').innerHTML = winsEx;
+        document.getElementById('lossEx').innerHTML = lossEx;
+        document.getElementById('drawEx').innerHTML = drawsEx;
+    }
+
+    return false;
+}
+
+function computerChooseExpansion(random) {
+    if (random === 1) {
+        return 'Rock';
+    } else if (random === 2) {
+        return 'Paper';
+    } else if (random === 3) {
+        return 'Scissors';
+    } else if(random === 4) {
+        return 'Lizard';
+    } else {
+        return 'Spock';
+    }
+
+}
+
+function compareExpansion(choice1, choice2) {
+    var txtDrawEx = "It's a draw";
+    var txtWinEx = 'You won!';
+    var txtLossEx = 'The computer won. Try again';
+
+    if (choice1 === choice2) {
+        drawsEx++;
+        return txtDrawEx;
+    } else if ((choice1 === 'Rock' && choice2 === 'Scissors') || 
+                    (choice1 === 'Rock' && choice2 === 'Lizard') ||
+                    (choice1 === 'Paper' && choice2 === 'Rock') || 
+                    (choice1 === 'Paper' && choice2 === 'Spock') ||
+                    (choice1 === 'Scissors' && choice2 === 'Paper') ||
+                    (choice1 === 'Scissors' && choice2 === 'Lizard') ||
+                    (choice1 === 'Lizard' && choice2 === 'Spock') ||
+                    (choice1 === 'Lizard' && choice2 === 'Paper') ||
+                    (choice1 === 'Spock' && choice2 === 'Scissors') ||
+                    (choice1 === 'Spock' && choice2 === 'Rock')
+                ) {
+        winsEx++;
+        return txtWinEx;
+    } else {
+        lossEx++;
+        return txtLossEx;
+    }
+}
---
+++ rockpaperscissors.js
@@ -0,0 +1,59 @@
+var wins = 0;
+var loss = 0;
+var draws = 0;
+
+function start() {
+    var random = Math.floor(Math.random() * 3 + 1);
+    var computerChoice = computerChoose(random);
+    var userChoice = null;
+
+    var radios = document.getElementsByName('option');
+    for (var i = 0, length = radios.length; i < length; i++) {
+        if (radios[i].checked) {
+            userChoice = radios[i].value;
+        }
+    }
+
+    if (userChoice === null) {
+        document.getElementById('userChoice').innerHTML = 'Please choose an option!';
+        document.getElementById('userChoice').style.color = 'red';
+    } else {
+        document.getElementById('userChoice').innerHTML = 'You chose: ' + userChoice;
+        document.getElementById('aiChoice').innerHTML = 'The computer chose: ' + computerChoice;
+
+        var winner = compare(userChoice, computerChoice);
+        document.getElementById('res').innerHTML = winner;
+        document.getElementById('win').innerHTML = wins;
+        document.getElementById('loss').innerHTML = loss;
+        document.getElementById('draw').innerHTML = draws;
+    }
+
+    return false;
+}
+
+function computerChoose(random) {
+    if (random === 1) {
+        return 'Rock';
+    } else if (random === 2) {
+        return 'Paper';
+    } else {
+        return 'Scissors';
+    }
+}
+
+function compare(choice1, choice2) {
+    var txtDraw = "It's a draw";
+    var txtWin = 'You won!';
+    var txtLoss = 'The computer won. Try again';
+
+    if (choice1 === choice2) {
+        draws++;
+        return txtDraw;
+    } else if ((choice1 === 'Rock' && choice2 === 'Scissors') || (choice1 === 'Paper' && choice2 === 'Rock') || (choice1 === 'Scissors' && choice2 === 'Paper')) {
+        wins++;
+        return txtWin;
+    } else {
+        loss++;
+        return txtLoss;
+    }
+}
